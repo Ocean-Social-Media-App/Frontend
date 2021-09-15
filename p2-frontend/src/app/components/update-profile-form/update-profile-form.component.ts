95a9883332d4e781c7707dbbe471aa47f0921b2b
trevdrury <trevdrury@gmail.com>
2021-09-15 14:25:41 -0700

    update profile form now prefills data in fields

--- p2-frontend/src/app/components/update-profile-form/update-profile-form.component.ts
+++ p2-frontend/src/app/components/update-profile-form/update-profile-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-update-profile-form',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatePostFormComponent implements OnInit {
 
-  constructor() { }
+  // variables to be set from session storage
+  userObj: any = {};
+
+  imageForm = this.fb.group({
+    image: [null]
+  })
+
+  updateProfileForm = this.fb.group({
+    username: [''],
+    email: [''],
+    firstName: [''],
+    lastName: [''],
+    password: [''],
+    proPicUrl: [''],
+    bday: [''],
+    aboutMe: ['']
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.userObj = JSON.parse(sessionStorage.getItem('userObj')!);
+    console.log(this.userObj);
+
+    this.updateProfileForm.get('username')?.setValue(this.userObj.username);
+    this.updateProfileForm.get('email')?.setValue(this.userObj.email);
+    this.updateProfileForm.get('firstName')?.setValue(this.userObj.firstName);
+    this.updateProfileForm.get('lastName')?.setValue(this.userObj.lastName);
+    this.updateProfileForm.get('proPicUrl')?.setValue(this.userObj.proPicUrl);
+    this.updateProfileForm.get('bday')?.setValue(this.userObj.bday);
+    this.updateProfileForm.get('aboutMe')?.setValue(this.userObj.aboutMe);
   }
 
 }
