0af4fcb846e819d2418c9d456168d9a55ef7107e
trevdrury <trevdrury@gmail.com>
2021-09-22 13:09:18 -0700

    added password reset page and routing

--- p2-frontend/src/app/services/user/user.service.ts
+++ p2-frontend/src/app/services/user/user.service.ts
@@ -22,34 +22,34 @@ export class UserService {
   }
 
   register(user: User): Observable<any> {
-    return this.httpCli.post(`${this.utilityService.getServerDomain}/api/user`, user, {withCredentials: true});
+    return this.httpCli.post(`${this.utilityService.getServerDomain()}/api/user`, user, {withCredentials: true});
   }
 
   createProfile(user: User): Observable<any> {
-    return this.httpCli.post(`${this.utilityService.getServerDomain}/api/createProfile`, user, {withCredentials: true});
+    return this.httpCli.post(`${this.utilityService.getServerDomain()}/api/createProfile`, user, {withCredentials: true});
   }
 
   updateProfile(user: User): Observable<any> {
-    return this.httpCli.put(`${this.utilityService.getServerDomain}/api/updateUser`, user);
+    return this.httpCli.put(`${this.utilityService.getServerDomain()}/api/updateUser`, user);
   }
 
   login(user: User): Observable<any> {
-    return this.httpCli.post(`${this.utilityService.getServerDomain}/api/login`, user, {withCredentials: true});
+    return this.httpCli.post(`${this.utilityService.getServerDomain()}/api/login`, user, {withCredentials: true});
   }
 
   logout() {
-    return this.httpCli.get(`${this.utilityService.getServerDomain}/api/logout`, {withCredentials: true});
+    return this.httpCli.get(`${this.utilityService.getServerDomain()}/api/logout`, {withCredentials: true});
   }
 
   addProfileImage(formData: FormData): Observable<any> {
-    return this.httpCli.post(`${this.utilityService.getServerDomain}/api/profile`, formData, {withCredentials: true});
+    return this.httpCli.post(`${this.utilityService.getServerDomain()}/api/profile`, formData, {withCredentials: true});
   }
 
   addPostImage(formData: FormData): Observable<any> {
-    return this.httpCli.post(`${this.utilityService.getServerDomain}/api/image`, formData, {withCredentials: true});
+    return this.httpCli.post(`${this.utilityService.getServerDomain()}/api/image`, formData, {withCredentials: true});
   }
 
   forgotPassword(username: string): Observable<any> {
-    return this.httpCli.get(`http://localhost:9000/api/forgot/${username}`, {withCredentials: true});
+    return this.httpCli.get(`${this.utilityService.getServerDomain()}/api/forgot/${username}`, {withCredentials: true});
   }
 }
