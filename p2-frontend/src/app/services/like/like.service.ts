dddbf698b277b5c05d8145a2a93a1b9d19bb03fb
gabrielgil245 <gabrielgil245@gmail.com>
2021-09-22 13:13:05 -0500

    injected placeholders in http requests to replace localhost:9000 with the variable of the server domain

--- p2-frontend/src/app/services/like/like.service.ts
+++ p2-frontend/src/app/services/like/like.service.ts
@@ -1,23 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Like } from 'src/app/models/Like';
+import { UtilityService } from '../utility.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LikeService {
 
-  constructor(private httpCli: HttpClient) { }
+  constructor(private httpCli: HttpClient, private utilityService: UtilityService) { }
 
   likePost(like: Like){
-    return this.httpCli.post(`http://localhost:9000/api/like`, like, {withCredentials: true});
+    return this.httpCli.post(`${this.utilityService.getServerDomain}/api/like`, like, {withCredentials: true});
   }
 
   unLikePost(likeId: number){
-    return this.httpCli.delete(`http://localhost:9000/api/like/${likeId}`, {withCredentials: true})
+    return this.httpCli.delete(`${this.utilityService.getServerDomain}/api/like/${likeId}`, {withCredentials: true})
   }
 
   checkLike(postId : number, userId : number){
-    return this.httpCli.get<any>(`http://localhost:9000/api/like/${postId}/${userId}`, {withCredentials: true})
+    return this.httpCli.get<any>(`${this.utilityService.getServerDomain}/api/like/${postId}/${userId}`, {withCredentials: true})
   }
 }
