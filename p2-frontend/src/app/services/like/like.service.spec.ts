cf889a08c5b8d053d120d9d7c0dc98213b3d0798
trevdrury <trevdrury@gmail.com>
2021-09-24 12:01:56 -0700

    added more tests and fixed some others

--- p2-frontend/src/app/services/like/like.service.spec.ts
+++ p2-frontend/src/app/services/like/like.service.spec.ts
@@ -4,20 +4,23 @@ import { Like } from 'src/app/models/Like';
 import { Post } from 'src/app/models/Post';
 import { User } from 'src/app/models/User';
 import { asyncData } from 'src/testing/async-observable-helpers';
+import { UtilityService } from '../utility.service';
 import { LikeService } from './like.service';
 
 describe('LikeService', () => {
   let likeService: LikeService;
+  let utilityService: UtilityService;
   let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [LikeService]
+      providers: [LikeService, UtilityService]
     });
 
     // inject the service
     likeService = TestBed.inject(LikeService);
+    utilityService = TestBed.inject(UtilityService);
     httpMock = TestBed.inject(HttpTestingController);
   });
 
@@ -26,7 +29,7 @@ describe('LikeService', () => {
       expect(result).toBe(true);
     });
 
-    const req = httpMock.expectOne('http://54.167.107.251:9000/api/like/1/2', 'call to api');
+    const req = httpMock.expectOne(`${utilityService.getServerDomain()}/api/feed/like/1/2`, 'call to api');
     expect(req.request.method).toBe('GET');
 
     req.flush(true);
@@ -39,7 +42,7 @@ describe('LikeService', () => {
       expect(result).toBe(1);
     });
 
-    const req = httpMock.expectOne('http://54.167.107.251:9000/api/like/1', 'call to unLikePost');
+    const req = httpMock.expectOne(`${utilityService.getServerDomain()}/api/feed/like/1`, 'call to unLikePost');
     expect(req.request.method).toBe('DELETE');
 
     req.flush(1);
@@ -47,12 +50,12 @@ describe('LikeService', () => {
     httpMock.verify();
   });
 
-  xit('should return like when likePost called', () => {
+  it('should return like when likePost called', () => {
     likeService.likePost(new Like()).subscribe((result: Like) => {
       expect(result).toEqual(new Like());
     });
 
-    const req = httpMock.expectOne('http://54.167.107.251:9000/api/like', 'call to likePost');
+    const req = httpMock.expectOne(`${utilityService.getServerDomain()}/api/feed/like`, 'call to likePost');
     expect(req.request.method).toBe('POST');
 
     req.flush(new Like());
