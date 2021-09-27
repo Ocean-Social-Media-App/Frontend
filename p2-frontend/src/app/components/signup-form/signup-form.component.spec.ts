4608e51c0dc5e635803e1f7f5730ad1def401d4c
gstamand <garrettstamand@gmail.com>
2021-09-27 14:22:28 -0500

    Added more tests

--- p2-frontend/src/app/components/signup-form/signup-form.component.spec.ts
+++ p2-frontend/src/app/components/signup-form/signup-form.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { SignupFormComponent } from './signup-form.component';
 
@@ -8,6 +12,10 @@ describe('SignupFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule],
+      providers: [
+        { provide: Router, useValue: null }
+      ],
       declarations: [ SignupFormComponent ]
     })
     .compileComponents();
@@ -22,4 +30,26 @@ describe('SignupFormComponent', () => {
   /* it('should create', () => {
     expect(component).toBeTruthy();
   }); */
+
+
+  it('should emit to parent when login here link is clicked', ()=>{
+
+    spyOn(component.toggle, 'emit');
+    const link = fixture.nativeElement.querySelector('a');
+    link.dispatchEvent(new Event('click'));
+ 
+    expect(component.toggle.emit).toHaveBeenCalled();
+
+  })
+
+  it('should call signUp when the button is clicked', ()=>{
+
+    spyOn(component, 'signUp');
+
+    const button = fixture.nativeElement.querySelector('app-buttons');
+    button.dispatchEvent(new Event('onClick'));
+
+    expect(component.signUp).toHaveBeenCalled();
+
+  })
 });
