ff98d45ff70644a7b191f453bf71fa9098906777
gstamand <garrettstamand@gmail.com>
2021-09-27 11:47:32 -0500

    Added testing for feed.component and search.component

--- p2-frontend/src/app/components/search/search.component.spec.ts
+++ p2-frontend/src/app/components/search/search.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { User } from 'src/app/models/User';
 
 import { SearchComponent } from './search.component';
 
@@ -8,6 +10,7 @@ describe('SearchComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ SearchComponent ]
     })
     .compileComponents();
@@ -15,6 +18,7 @@ describe('SearchComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
+    window.sessionStorage.setItem('userObj', JSON.stringify(new User()))
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -22,4 +26,36 @@ describe('SearchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display all users in listTemp', ()=>{
+
+    let user0 = new User();
+    user0.username = "Adam";
+    let user1 = new User();
+    user1.username = "Dave";
+    let user2 = new User();
+    user2.username = "Abby";
+    let user3 = new User();
+    user3.username = "Amanda";
+
+    component.userList.push(user0);
+    component.userList.push(user1);
+    component.userList.push(user2);
+    component.userList.push(user3);
+
+    component.searchInput = "A"
+    component.ngDoCheck();
+
+    fixture.detectChanges();
+
+    expect(document.getElementsByTagName("app-user-link").length).toBe(3);
+
+    component.searchInput = "D"
+    component.ngDoCheck();
+
+    fixture.detectChanges();
+
+    expect(document.getElementsByTagName("app-user-link").length).toBe(1);
+  })
+
 });
