7c0ac1cf92dd95c4a6c9524b662ae10ea3ee8b65
gabrielgil245 <gabrielgil245@gmail.com>
2021-09-28 16:31:29 -0500

    action menu buttons now toggle the display for the new post form component and the notification component

--- p2-frontend/src/app/components/action-menu/action-menu.component.ts
+++ p2-frontend/src/app/components/action-menu/action-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-action-menu',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionMenuComponent implements OnInit {
 
+  @Output()
+  eventFromActionMenu: EventEmitter<boolean> = new EventEmitter();
+
+  @Output()
+  eventFromActionNotification: EventEmitter<boolean> = new EventEmitter();
+
+  _postCheck: boolean = false;
+  _notificationCheck: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  userIsPosting() {
+    this._notificationCheck = false;
+    this._postCheck = !this._postCheck;
+    console.log(this._postCheck);
+    this.eventFromActionMenu.emit(this._postCheck);
+  }
+
+  notificationsDisplay() {
+    this._postCheck = false;
+    this._notificationCheck = !this._notificationCheck;
+    console.log(this._notificationCheck);
+    this.eventFromActionNotification.emit(this._notificationCheck);
   }
 
 }
--- p2-frontend/src/app/pages/user-feed/user-feed.component.ts
+++ p2-frontend/src/app/pages/user-feed/user-feed.component.ts
@@ -13,6 +13,8 @@ export class UserFeedComponent implements OnInit {
 
   pageCount = 0;
   userObj = {};
+  _userIsPosting: boolean = false;
+  _notificationsDisplay: boolean = false;
 
   constructor(private router: Router) { }
 
@@ -28,6 +30,16 @@ export class UserFeedComponent implements OnInit {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
       this.pageCount++;
     }
+  }
+
+  receivePostStatus(postCheck: boolean) {
+    this._notificationsDisplay = false;
+    this._userIsPosting = postCheck;
+  }
+
+  receiveNotificationStatus(notificationCheck: boolean) {
+    this._userIsPosting = false;
+    this._notificationsDisplay = notificationCheck;
   }
 
 }
