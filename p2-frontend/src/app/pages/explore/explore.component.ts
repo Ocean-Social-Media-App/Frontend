6828085f50bf5f5e0868b08fed46274baac3b9ec
Angel Walker <55213253+AngelWalker06@users.noreply.github.com>
2021-10-01 21:37:40 -0400

    working on explore page

--- p2-frontend/src/app/pages/explore/explore.component.ts
+++ p2-frontend/src/app/pages/explore/explore.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, Input, OnInit } from '@angular/core';
+
 import { Subscriber } from 'rxjs';
 import { User } from 'src/app/models/User';
 import { UserService } from 'src/app/services/user/user.service';
@@ -9,9 +10,12 @@ import { UserService } from 'src/app/services/user/user.service';
   styleUrls: ['./explore.component.css']
 })
 export class ExploreComponent implements OnInit {
-
+  userview: number = 0;
   pageCount = 1;
-
+  aUser= {
+    user: {userId : this.userview }
+  }
+  
   @Input()
   user: User = {
     userId: undefined,
@@ -24,8 +28,9 @@ export class ExploreComponent implements OnInit {
     bday: undefined,
     proPicUrl: undefined
   }
-
-  userId: number;
+  
+  
+  userId: number = 0;
   userIdFollow: number;
   userList: Array<any> = []; 
   wholeList: Array<any> = []; 
@@ -36,19 +41,60 @@ export class ExploreComponent implements OnInit {
   following: any[];
   followingUsers: any[] = [];
   allFollowing: Subscriber<any> = new Subscriber;
+  arrayOfFollowers: any[] = [];
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.userId = JSON.parse(sessionStorage.getItem('userObj')).userId;
 
+    this.followed = false;
+
     this.userService.getAllUsers().subscribe(users => {
       this.wholeList = users.data;
       this.userList = this.wholeList.slice(0, this.listNum)
       console.log(this.userList)
+
+      for(let user of this.wholeList){
+        this.userService.getUserById(user.userId).subscribe(follows =>{
+          console.log(follows)
+          follows.data.followers.forEach(element => {
+            console.log(element)
+           this.userService.getUserById(element).subscribe(followerUser => {
+           this.arrayOfFollowers.push(followerUser);
+         })
+        if(element == this.userId){
+          /* this.followLabel = "Unfollow"; */
+          console.log(element)
+          console.log(this.userId)
+          this.followed = true;
+        }else{
+          this.followed = false;
+        }
+      });
+    })
+      }
     })
 
-    this.userService.getUserById(this.userId).subscribe(responseData => {
+   
+
+    
+    /* this.userService.getUserById(this.userIdFromParam).subscribe(follows =>{
+      follows.data.followers.forEach(element => {
+        this.userService.getUserById(element).subscribe(followerUser => {
+          this.arrayOfFollowers.push(followerUser);
+        })
+       if(element == this.userInSession){
+         this.followLabel = "Unfollow";
+         this.followed = true;
+       }
+     });
+     this.following = follows.data.user_following.length;
+     this.followers = follows.data.followers.length;
+
+   }) */
+
+   /*  this.userService.getUserById(this.userId).subscribe(responseData => {
       this.following = responseData.data.user_following;
     })
     setTimeout(()=>{
@@ -57,7 +103,9 @@ export class ExploreComponent implements OnInit {
        this.followingUsers.push(specificUser.data); 
       }
     })})
-    }, 500)
+    }, 500) */
+
+
   }
 
   
@@ -70,19 +118,14 @@ export class ExploreComponent implements OnInit {
      this.userService.followUser(userIdFollow, this.userId).subscribe(responseData =>{
       console.log(responseData) 
       if(responseData.success){
-        console.log("trying to unfollow")
-         this.followLabel = "Unfollow"
          console.log("followed user")
-       }else{
-         console.log(responseData);
-
        }
     })
-  }
+    }
     else{
       this.userService.unfollowUser(userIdFollow, this.userId).subscribe(response=>{
         if(response.success){
-          this.followLabel = "Follow"
+          /* this.followLabel = "Follow" */
           console.log("unfollowed user")
         }}
     )}
